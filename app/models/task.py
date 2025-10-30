68e0c78ce03314512ff569fccb17721997462ee1
Veema Jhagru <veemajhagru@gmail.com>
2025-10-30 18:11:47 -0400

    added to task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    completed_at: Mapped[datetime] = mapped_column(nullable=True, server_default=None)
