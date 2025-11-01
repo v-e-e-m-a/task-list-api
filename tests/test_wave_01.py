072b3619fe8842ae6dcf0d1c9fade1d279a6aefb
Veema Jhagru <veemajhagru@gmail.com>
2025-11-01 00:51:23 -0400

    small changes

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -152,7 +152,7 @@ def test_get_task_not_found(client):
     assert response_body == {"message": "task 1 not found"}
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_task(client):
     # Act
     response = client.post("/tasks", json={
